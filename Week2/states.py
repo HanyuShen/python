300fc68052a0e5dec299a6d2e030e2941e75353c
w55186hs <hanyu.shen@student.manchester.ac.uk>
2020-10-17 19:40:02 +0100

    assign a value

--- Week2/states.py
+++ Week2/states.py
@@ -1,17 +1,2 @@
-day = "Beautiful"
-print(day[1])
-
-day = "Beautiful"
-print(day[0:5])
-
-day = "Beautiful"
-print(day[-3])
-print(day[-3:])
-print(day[-5:3])
-print(day[-5:-3])
-
-day == "Beautiful"
-print ("Today is "+day)
-
-day="Beautiful"
-print(str(day == "Beautiful")+":Todat is "+day)
+operand1 = input("Input a number: ")
+print ("You entered " + operand1)
