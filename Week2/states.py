f79f7fe489014b752df050b07317742f5ae9f347
w55186hs <hanyu.shen@student.manchester.ac.uk>
2020-10-17 18:53:16 +0100

    Boolean

--- Week2/states.py
+++ Week2/states.py
@@ -1,14 +1,10 @@
 def printState():
-    print ("Red Light is " + str(redLight))
-    print ("Yellow Light is " + str(yellowLight))
-    print ("Green Light is " + str(greenLight))
-redLight = 1
-yellowLight = 0
-greenLight = 0
-
+    print ("Red Light on? " + str(redLight))
+    print ("Yellow Light on? " + str(yellowLight))
+    print ("Green Light on? " + str(greenLight))
+redLight = True
+yellowLight = False
+greenLight = False
 printState()
-print (type(redLight))
 
-print(3+5)
-print("3"+"5")
-print(int("3")+5)
+print (type(redLight))
