914e0a6d20b51560955548d1629e7c2d55facd89
Hanyu Shen <hanyu.shen@student.manchester.ac.uk>
2020-11-14 10:27:39 +0000

    word row column direction

--- Week6/WordSearch game.py
+++ Week6/WordSearch game.py
@@ -1,3 +1,4 @@
+import random
 
 word1 = "happy"
 word2 = "sunny"
@@ -38,4 +39,57 @@ rowMax = int(input("Enter number of rows: "))
 colMax = int(input("Enter number of columns: "))
 grid = GenerateGrid()
 DisplayGrid()
+
+
+def PlaceWords():
+    for word in words:
+        if len(word) > rowMax or len(word) > colMax : continue
+        direction = random.randint(0,3)
+        if direction == 0:
+            print("placing",word,"from left to right")
+            min = 0
+            max = colMax - len(word)
+        elif direction == 1:
+            print("placing",word,"from right to left")
+            min = len(word) - 1
+            max = colMax - 1
+        elif direction == 2:
+            print("placing",word,"from toop to bottom")
+            min = 0
+            max = rowMax - len(word)
+        elif direction == 3:
+            print("placing",word,"from bottom to top")
+            min = len(word) - 1
+            max = rowMax - 1
+        print("Word length is ",len(word),"so: ")
+        print("min: ",min,"max: ",max)
+        square = random.randint(min,max)
+        print("Square chosen is ",square)
+        if direction < 2:
+            row = random.randint(0,rowMax-1)
+            col = square
+            print(" in row ",row)
+        else:
+            col = random.randint(0,colMax-1)
+            row = square
+            print(" in column ", col)
+
+PlaceWords()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
