6c4390027a8b316046250560dc6f791bfbed59a5
Hanyu Shen <hanyu.shen@student.manchester.ac.uk>
2020-11-14 10:08:26 +0000

    make a grid

--- Week6/WordSearch game.py
+++ Week6/WordSearch game.py
@@ -8,33 +8,34 @@ def GenerateGrid():
     grid = [['-' for x in range(colMax)] for y in range(rowMax)]
     return grid
 
-
-def DisplayGrid():
-    for row in range(rowMax):
-        for col in range(colMax):
-            print(grid[row][col] + ' ', end="")
-        print()
 def GetWords():
     theWords = ["happy","cheerful","chipper","effervescent","jaunty","jolly"]
     return theWords
 
-def GetWords():
+def GetWordsFromUser():
     theWords = []
     while True:
-        word = input("Enter a word and press <enter> (or just press <enter> to finish) :")
+        word = input("Enter a word and press <enter> (or just press <enter> to finish): ")
         if word != "": theWords.append(word)
         else:return theWords
+     
+words = GetWords()
 
 def DisplayWords():
     for word in words:
         print(word)
 
-words = GetWords()
-words = DisplayWords()
+DisplayWords()
 
+def DisplayGrid():
+    for row in range(rowMax):
+        for col in range(colMax):
+            print(grid[row][col] + ' ',end="")
+        print()
 
 
 rowMax = int(input("Enter number of rows: "))
 colMax = int(input("Enter number of columns: "))
 grid = GenerateGrid()
 DisplayGrid()
+
