6dccf607577d24c4b8dbd1840e93f7bc45e03fe0
Hanyu Shen <hanyu.shen@student.manchester.ac.uk>
2020-11-26 16:13:55 +0000

    create the initial state

--- Week7/oxo.py
+++ Week7/oxo.py
@@ -1,38 +1,26 @@
-from tkinter import Tk,PhotoImage,Button,messagebox
+from tkinter import Tk,PhotoImage,Button
 
 def create_buttons():
-    square[0] = Button(window,image=available_space,width="100",
-                       height="100",command=lambda: handle_button_click(0))
-
+    square[0] = Button(window,image=available_space,width="100",height="100",command=None)
     square[0].place(x=0,y=0)
-
-    square[1] = Button(window,image=player1_taken,width="100",
-                       height="100",command=lambda: handle_button_click(1))
-
-    square[1].place(x=100,y=100)
-
-    square[2] = Button(window,image=player2_taken,width="100",
-                       height="100",command=lambda: handle_button_click(2))
-
-    square[2].place(x=200,y=200)
-
-counter = 0
-
-def square_taken():
-    messagebox.showinfo("Square Taken","Square already taken choose another!")
-
-def handle_button_click(button_number):
-    print("Button ",button_number, "was clicked")
-    global counter
-    if counter % 2 == 0:
-        square[button_number].configure(image=player1_taken,command=square_taken)
-    else:
-        square[button_number].configure(image=player2_taken,command=square_taken)
-
-counter += 1
-
-
- 
+    square[1] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[1].place(x=0,y=100)
+    square[2] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[2].place(x=0,y=200)
+    square[3] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[3].place(x=100,y=0)
+    square[4] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[4].place(x=100,y=100)
+    square[5] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[5].place(x=100,y=200)
+    square[6] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[6].place(x=200,y=0)
+    square[7] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[7].place(x=200,y=100)
+    square[8] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[8].place(x=200,y=200)
+    
+  
 window = Tk()
 window.title("OXO Game")
 window.geometry("300x300")
@@ -44,10 +32,16 @@ player1_taken = PhotoImage(file="myButtonP1.png")
 player2_taken = PhotoImage(file="myButtonP2.png")
 winner = PhotoImage(file="winner.png")
 
-
-
 square = [None]*9
 create_buttons()
+
+
+
+
+
+
+
+
 
 
 window.mainloop()
