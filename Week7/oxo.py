a3f479c7bce6eb330ec6d8251acceccb2c183fe8
Hanyu Shen <hanyu.shen@student.manchester.ac.uk>
2020-11-26 16:23:17 +0000

    event handling

--- Week7/oxo.py
+++ Week7/oxo.py
@@ -1,24 +1,31 @@
-from tkinter import Tk,PhotoImage,Button
+from tkinter import Tk,PhotoImage,Button,messagebox
 
 def create_buttons():
-    square[0] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[0] = Button(window,image=available_space,width="100",height="100",command= lambda:handle_button_click(0))
     square[0].place(x=0,y=0)
-    square[1] = Button(window,image=available_space,width="100",height="100",command=None)
-    square[1].place(x=0,y=100)
-    square[2] = Button(window,image=available_space,width="100",height="100",command=None)
-    square[2].place(x=0,y=200)
-    square[3] = Button(window,image=available_space,width="100",height="100",command=None)
-    square[3].place(x=100,y=0)
-    square[4] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[1] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(1))
+    square[1].place(x=100,y=0)
+    square[2] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(2))
+    square[2].place(x=200,y=0)
+    square[3] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(3))
+    square[3].place(x=0,y=100)
+    square[4] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(4))
     square[4].place(x=100,y=100)
-    square[5] = Button(window,image=available_space,width="100",height="100",command=None)
-    square[5].place(x=100,y=200)
-    square[6] = Button(window,image=available_space,width="100",height="100",command=None)
-    square[6].place(x=200,y=0)
-    square[7] = Button(window,image=available_space,width="100",height="100",command=None)
-    square[7].place(x=200,y=100)
-    square[8] = Button(window,image=available_space,width="100",height="100",command=None)
+    square[5] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(5))
+    square[5].place(x=200,y=100)
+    square[6] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(6))
+    square[6].place(x=0,y=200)
+    square[7] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(7))
+    square[7].place(x=100,y=200)
+    square[8] = Button(window,image=available_space,width="100",height="100",command=lambda:handle_button_click(8))
     square[8].place(x=200,y=200)
+
+def square_taken():
+    messagebox.showinfo("Square Taken","Square already taken chhose another!")
+
+def handle_button_click(button_number):
+    print("Button ", button_number,"was clicked")
+    square[button_number].configure(image=player1_taken,command=square_taken)
     
   
 window = Tk()
